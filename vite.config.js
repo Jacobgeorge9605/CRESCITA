134f696f06338123ec5ca63c59eb59f4741b3df6
Jacob George <jacobgeorge9605@gmail.com>
2025-01-13 15:07:54 +0530

    final changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +9,13 @@ export default defineConfig({
     alias: [
       {
         find: './runtimeConfig',
-        replacement: './runtimeConfig.browser'
-      }
-    ]
-  }
-})
+        replacement: './runtimeConfig.browser',
+      },
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: ['./faq'], // Specify external modules here
+    },
+  },
+});
